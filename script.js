4c6d57911a8f974ecd3b74d214d609a8f6bf3c2b
Daniela Oquendo Tabares <DanielaOquendo@comfama.com.co>
2023-06-28 11:07:01 -0500

    taller Js Uno

---
+++ script.js
@@ -0,0 +1,17 @@
+const contenedor= document.getElementById('contenedor')
+const cuadroUno= document.getElementById('cuadroUno')
+const cuadroDos= document.getElementById('cuadroDos')
+const redes = document.getElementById('redes')
+const botonIn = document.getElementById('botonIn')
+const cuadrito= document.getElementById('cuadrito')
+
+
+const cambiar= () =>{
+    contenedor.classList.add('contenedor')
+    cuadroUno.classList.add('cuadroUno')
+    cuadroDos.classList.add('cuadroDos')
+    redes.classList.add('redes')
+    botonIn.classList.add('botonIn')
+    cuadrito.classList.add('cuadrito')
+    }
+
